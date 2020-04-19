e906363d3146f8c2ea703743c56e7071ce8cdcc5
Kislay Verma <kislayv@uber.com>
2020-04-19 23:15:38 +0530

    WIP - UI to display all loaded rule engines

--- src/main/java/com/github/kislayverma/rulette/rest/ui/HomeController.java
+++ src/main/java/com/github/kislayverma/rulette/rest/ui/HomeController.java
@@ -1,14 +1,23 @@
 package com.github.kislayverma.rulette.rest.ui;
 
+import com.github.kislayverma.rulette.core.metadata.RuleSystemMetaData;
+import com.github.kislayverma.rulette.rest.rulesystem.RuleSystemController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
+    @Autowired
+    private RuleSystemController ruleSystemController;
+
     @GetMapping("/")
-    public String homePage(Model model) {
-        return "home";
+    public String showAllRuleSystem(Model model) {
+        model.addAttribute("ruleSystems", ruleSystemController.getAllRuleSystemMetaData());
+        return "rule-systems";
     }
 }
