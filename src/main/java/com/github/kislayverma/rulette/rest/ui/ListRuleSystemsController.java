87b70780edb87635ff284ecfdbe4ab9bc8cef9b5
Kislay Verma <kislayv@uber.com>
2020-05-03 01:23:53 +0530

    Reload button on rule system list page

--- src/main/java/com/github/kislayverma/rulette/rest/ui/ListRuleSystemsController.java
+++ src/main/java/com/github/kislayverma/rulette/rest/ui/ListRuleSystemsController.java
@@ -1,15 +1,19 @@
 package com.github.kislayverma.rulette.rest.ui;
 
 import com.github.kislayverma.rulette.rest.rulesystem.RuleSystemService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/ui")
 public class ListRuleSystemsController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ListRuleSystemsController.class);
 
     @Autowired
     private RuleSystemService ruleSystemService;
@@ -20,5 +24,12 @@ public class ListRuleSystemsController {
                                     @RequestParam(defaultValue = "50") Integer pageSize) {
         model.addAttribute("ruleSystemPage", ruleSystemService.getAllRuleSystemMetaData(pageNum, pageSize));
         return "rule-systems";
+    }
+
+    @RequestMapping("/{ruleSystemName}/reload")
+    public String reloadRuleSystem(Model model, @PathVariable String ruleSystemName) {
+        LOGGER.info("Reloading rule system {}", ruleSystemName);
+        ruleSystemService.reload(ruleSystemName);
+        return "redirect:/ui";
     }
 }
