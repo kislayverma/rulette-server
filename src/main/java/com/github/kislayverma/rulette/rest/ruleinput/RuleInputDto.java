54ba188ba8a9cbcb2b9e98a0007926500413ee5a
Kislay Verma <kislayv@uber.com>
2020-05-04 22:58:18 +0530

    UI to add new rule input to a rule system

---
+++ src/main/java/com/github/kislayverma/rulette/rest/ruleinput/RuleInputDto.java
@@ -0,0 +1,74 @@
+package com.github.kislayverma.rulette.rest.ruleinput;
+
+import com.github.kislayverma.rulette.core.ruleinput.type.RuleInputType;
+
+import java.io.Serializable;
+
+public class RuleInputDto implements Serializable {
+    private String name;
+    private int priority;
+    private RuleInputType ruleInputType;
+    private String dataType;
+    private String rangeLowerBoundFieldName;
+    private String rangeUpperBoundFieldName;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public RuleInputType getRuleInputType() {
+        return ruleInputType;
+    }
+
+    public String getDataType() {
+        return dataType;
+    }
+
+    public String getRangeLowerBoundFieldName() {
+        return rangeLowerBoundFieldName;
+    }
+
+    public String getRangeUpperBoundFieldName() {
+        return rangeUpperBoundFieldName;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public void setRuleInputType(RuleInputType ruleInputType) {
+        this.ruleInputType = ruleInputType;
+    }
+
+    public void setDataType(String dataType) {
+        this.dataType = dataType;
+    }
+
+    public void setRangeLowerBoundFieldName(String rangeLowerBoundFieldName) {
+        this.rangeLowerBoundFieldName = rangeLowerBoundFieldName;
+    }
+
+    public void setRangeUpperBoundFieldName(String rangeUpperBoundFieldName) {
+        this.rangeUpperBoundFieldName = rangeUpperBoundFieldName;
+    }
+
+    @Override
+    public String toString() {
+        return "RuleInputDto{" +
+            "name='" + name + '\'' +
+            ", priority=" + priority +
+            ", ruleInputType=" + ruleInputType +
+            ", dataType='" + dataType + '\'' +
+            ", rangeLowerBoundFieldName='" + rangeLowerBoundFieldName + '\'' +
+            ", rangeUpperBoundFieldName='" + rangeUpperBoundFieldName + '\'' +
+            '}';
+    }
+}
--- src/main/java/com/github/kislayverma/rulette/rest/ui/EditRuleInputController.java
+++ src/main/java/com/github/kislayverma/rulette/rest/ui/EditRuleInputController.java
@@ -1,6 +1,7 @@
 package com.github.kislayverma.rulette.rest.ui;
 
-import com.github.kislayverma.rulette.rest.rule.RuleService;
+import com.github.kislayverma.rulette.core.metadata.RuleInputMetaData;
+import com.github.kislayverma.rulette.rest.ruleinput.RuleInputDto;
 import com.github.kislayverma.rulette.rest.ruleinput.RuleInputService;
 import com.github.kislayverma.rulette.rest.rulesystem.RuleSystemService;
 import org.slf4j.Logger;
@@ -8,8 +9,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -18,19 +21,53 @@ public class EditRuleInputController {
     private static final Logger LOGGER = LoggerFactory.getLogger(EditRuleInputController.class);
 
     @Autowired
+    private RuleInputService ruleInputService;
+
+    @Autowired
     private RuleSystemService ruleSystemService;
 
-    @Autowired
-    private RuleService ruleService;
+    @RequestMapping("/{ruleSystemName}/ruleinput/add")
+    public String showAddRuleInput(
+        Model model, @PathVariable String ruleSystemName) {
+        RuleInputDto ruleInputToAdd = new RuleInputDto();
+        model.addAttribute("ruleSystem", ruleSystemService.getRuleSystemMetadata(ruleSystemName));
+        model.addAttribute("ruleInputToAdd", ruleInputToAdd);
 
-    @Autowired
-    private RuleInputService ruleInputService;
+        return "rule-input";
+    }
+
+    @RequestMapping(value="/{ruleSystemName}/ruleinput/save", method= RequestMethod.POST)
+    public String addRuleInput(
+        @ModelAttribute("ruleInputToAdd") RuleInputDto ruleInputDto, @PathVariable String ruleSystemName, RedirectAttributes redirectAttributes) {
+        try {
+            LOGGER.info(ruleInputDto.toString());
+            RuleInputMetaData rimd = new RuleInputMetaData(
+                ruleInputDto.getName(),
+                ruleInputDto.getPriority(),
+                ruleInputDto.getRuleInputType(),
+                ruleInputDto.getDataType(),
+                ruleInputDto.getRangeLowerBoundFieldName(),
+                ruleInputDto.getRangeUpperBoundFieldName());
+            ruleInputService.addRuleInput(ruleSystemName, rimd);
+
+            redirectAttributes.addFlashAttribute("message", "Successfully added rule input! Reload the rule system to deploy the new state");
+            redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+
+            return "redirect:/ui";
+        } catch (Exception e) {
+            LOGGER.error("Error saving new rule input", e);
+            redirectAttributes.addFlashAttribute("message", "Error adding rule input! " + e.getMessage());
+            redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+
+            return "redirect:/ui/{ruleSystemName}/ruleinput/add";
+        }
+    }
 
     @RequestMapping("/{ruleSystemName}/ruleinput/{ruleInputName}/delete")
     public String deleteRuleInput(
         Model model, @PathVariable String ruleSystemName, @PathVariable String ruleInputName, RedirectAttributes redirectAttributes) {
         ruleInputService.deleteRuleInput(ruleSystemName, ruleInputName);
-        redirectAttributes.addFlashAttribute("message", "Successfully deleted ruleinput! Reload the rule system to deploy the new state");
+        redirectAttributes.addFlashAttribute("message", "Successfully deleted rule input! Reload the rule system to deploy the new state");
         redirectAttributes.addFlashAttribute("alertClass", "alert-success");
 
         return "redirect:/ui";
